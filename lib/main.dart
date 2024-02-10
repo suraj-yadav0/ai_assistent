e503c17f5589ce0abfe95654f62a0c718f1cfd55
Suraj Yadav <90672206+suraj-yadav0@users.noreply.github.com>
2024-02-10 18:30:02 +0530

    issue resolved

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:ai_assistent/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future <void> main() async {
 WidgetsFlutterBinding.ensureInitialized();
 
-//Hive.defaultDirectory = (await  getApplicationDocumentsDirectory()).path;
+Hive.defaultDirectory = (await  getApplicationDocumentsDirectory()).path;
 await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
