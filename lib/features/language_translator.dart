e3334d5c0b6c01358a70adcd99f0d98ac606c730
Suraj Yadav <90672206+suraj-yadav0@users.noreply.github.com>
2024-03-03 11:16:28 +0530

    completed the UI for the Language Translator

--- lib/features/language_translator.dart
+++ lib/features/language_translator.dart
@@ -1,3 +1,6 @@
+import 'package:ai_assistent/controllers/translator_controlller.dart';
+import 'package:ai_assistent/helper/global.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LanguageTranslator extends StatefulWidget {
@@ -8,16 +11,101 @@ class LanguageTranslator extends StatefulWidget {
 }
 
 class _LanguageTranslatorState extends State<LanguageTranslator> {
+  final _c = TranslatorController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('AI Language Translator'),
-
       ),
-
       body: ListView(
-        children: [],
+        physics: const BouncingScrollPhysics(),
+        padding: EdgeInsets.only(
+          top: mq.height * 0.02,
+          bottom: mq.height * .01,
+        ),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // from language
+              Container(
+                height: 50,
+                alignment: Alignment.center,
+                width: mq.width * .4,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+                child: const Text("Auto"),
+              ),
+
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  CupertinoIcons.repeat,
+                  color: Colors.grey,
+                ),
+              ),
+// TO Button,
+
+              Container(
+                height: 50,
+                alignment: Alignment.center,
+                width: mq.width * .4,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+                child: const Text("TO"),
+              )
+            ],
+          ),
+
+          // for input
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: mq.width * 0.04, vertical: mq.height * 0.035),
+            child: TextFormField(
+                controller: _c.texc,
+              minLines: 5,
+              maxLines: null,
+              textAlign: TextAlign.center,
+              onTapOutside: (event) => FocusScope.of(context).unfocus(),
+              decoration: const InputDecoration(
+                  hintText: "Translate Anything You Want!",
+                  hintStyle: TextStyle(fontSize: 13.5),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)))),
+            ),
+          ),
+
+           SizedBox(height: mq.height * 0.04,),
+          // for output,
+if(_c.resultc.text.isNotEmpty)
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: mq.width * 0.04, vertical: mq.height * 0.035),
+            child: TextFormField(
+               controller: _c.resultc ,
+              // minLines: 5,
+              maxLines: null,
+              textAlign: TextAlign.center,
+              onTapOutside: (event) => FocusScope.of(context).unfocus(),
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)))),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/language_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LanguageSheet extends StatelessWidget {
+  const LanguageSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
